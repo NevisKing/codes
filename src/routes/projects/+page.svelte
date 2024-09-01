a0429a52ce79b94067707f530a07a04fd2957271
NevisKing <nevisdontforget@gmail.com>
2024-09-01 13:11:08 +0200

    fixed

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,3 +2,5 @@
 This will be the new version of the smash leaderboard if you remember that. It does almost nothing right now.
 <a href="/">Personal website</a>
 What did you expect?
+<a href="/projects/thesis">WDSDM Optical Networking</a>
+This was my bachelors thesis project, its a bit heady but if you're curious you can read it here.
---
+++ src/routes/projects/thesis/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import pdfUrl from '$lib/images/BSc_Project.pdf';
+</script>
+
+<iframe src={pdfUrl} width="100%" height="1000" title="PDF"/>
