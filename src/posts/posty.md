8c2cbadbd1421e5177a28f1a114e21f8327a20d8
NevisKing <nevisdontforget@gmail.com>
2024-10-06 19:10:58 +0200

    blog test

---
+++ src/posts/posty.md
@@ -0,0 +1,11 @@
+---
+title: First post
+description: Inaugural hello world type post
+date: '2024-10-06'
+categories:
+  - gen
+published: true
+---
+# Ok so now there is a post
+thats cool
+![Svelte](/dog.png)
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,47 @@
-not yet &lt3
+<script lang="ts">
+
+    export let data
+</script>
+
+
+<!-- Posts -->
+<section>
+    <ul class="posts">
+        {#each data.posts as post}
+            <li class="post">
+                <a href={'blog/'+post.slug} class="title">{post.title}</a>
+                <p class="date">{(post.date)}</p>
+                <p class="description">{post.description}</p>
+            </li>
+        {/each}
+    </ul>
+</section>
+
+<style>
+    .posts {
+        display: grid;
+        gap: 2rem;
+    }
+
+    .post {
+        max-inline-size: var(--size-content-3);
+    }
+
+    .post:not(:last-child) {
+        border-bottom: 1px solid var(--border);
+        padding-bottom: var(--size-7);
+    }
+
+    .title {
+        font-size: var(--font-size-fluid-3);
+        text-transform: capitalize;
+    }
+
+    .date {
+        color: var(--text-2);
+    }
+
+    .description {
+        margin-top: var(--size-3);
+    }
+</style>
